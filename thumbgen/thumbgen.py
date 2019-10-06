1ecb6073a89f66cf6a9494267b434d045fb75402
Mudassir Khan <mudassirkhan19@gmail.com>
2019-10-06 21:19:39 +0530

    renames package * replaces few logging statements with print * changes default worker count

--- thumbgen/thumbgen.py
+++ thumbgen/thumbgen.py
@@ -1,24 +1,25 @@
 import os
 import sys
-from multiprocessing import Pool, cpu_count
+from multiprocessing import Pool
 from pathlib import Path
+from typing import Union
 
 import click
 import gi
+from loguru import logger
+from tqdm import tqdm
 
 gi.require_version("GnomeDesktop", "3.0")
 from gi.repository import Gio, GnomeDesktop
-from loguru import logger
-from tqdm import tqdm
 
 
 global factory
 factory = GnomeDesktop.DesktopThumbnailFactory()
 logger.remove()
-logger.add(sys.stderr, level="INFO")
+logger.add(sys.stdout, level="INFO")
 
 
-def make_thumbnail(fpath):
+def make_thumbnail(fpath: Path) -> bool:
     mtime = os.path.getmtime(fpath)
     # Use Gio to determine the URI and mime type
     f = Gio.file_new_for_path(str(fpath))
@@ -44,9 +45,9 @@ def make_thumbnail(fpath):
     return True
 
 
-def thumbnail_folder(*, dir_path, workers):
+def thumbnail_folder(*, dir_path: Union[str, Path], workers: int):
     all_images = get_all_images(dir_path=dir_path)
-    with Pool(processes=int(workers)) as p:
+    with Pool(processes=workers) as p:
         list(tqdm(p.imap(make_thumbnail, all_images), total=len(all_images)))
 
 
@@ -55,7 +56,7 @@ def get_all_images(*, dir_path):
     img_suffixes = [".jpg", ".jpeg", ".png"]
     all_files = Path(dir_path).rglob("*")
     all_images = [fpath for fpath in all_files if fpath.suffix in img_suffixes]
-    logger.info(f"Found {len(all_images)} images in the directory: {dir_path}")
+    print(f"Found {len(all_images)} images in the directory: {dir_path.resolve()}")
     return all_images
 
 
@@ -64,16 +65,17 @@ def check_valid_directory(*, dir_path):
         raise ValueError(f"{dir_path} doesn't exists or isn't a valid directory")
 
 
+@logger.catch()
 @click.command()
 @click.option(
     "-d", "--img_dirs", help='directories to recursively generate thumbnails seperated by space, eg: "dir1/dir2 dir3"'
 )
-@click.option("-w", "--workers", default=cpu_count() - 1, help="no of cpus to use for processing")
+@click.option("-w", "--workers", default=1, help="no of cpus to use for processing")
 def main(img_dirs, workers):
     img_dirs = [Path(img_dir) for img_dir in img_dirs.split()]
     for img_dir in img_dirs:
-        thumbnail_folder(dir_path=img_dir, workers=workers)
-    logger.info("Thumbnail Generation Completed!")
+        thumbnail_folder(dir_path=img_dir, workers=int(workers))
+    print("Thumbnail Generation Completed!")
 
 
 if __name__ == "__main__":
